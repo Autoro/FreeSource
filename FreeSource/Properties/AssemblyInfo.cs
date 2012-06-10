dd1093505b9b00a5f85d8ae1371b322cf8da47cc
Michael J. Gray <mgray@emitcode.com>
2012-06-10 10:58:53 -0700

    First commit.

---
+++ FreeSource/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("FreeSource")]
+[assembly: AssemblyDescription("An application which adds any open source initiative approved license to your source files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EmitCode Corporation")]
+[assembly: AssemblyProduct("FreeSource")]
+[assembly: AssemblyCopyright("Copyright © EmitCode Corporation 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0a3a7936-d1a1-416d-adfc-f2a3e48313ca")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
